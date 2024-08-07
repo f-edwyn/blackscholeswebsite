75f6ce3649690d2a28e98fc92aaeef6dd21a0959
Edwyn Flores <edwynflores@gmail.com>
2024-08-06 18:17:09 -0700

    Completed call and put valuation displays along with adjusting css errors for input variables

--- blackscholeswebsite/static/js/main.js
+++ blackscholeswebsite/static/js/main.js
@@ -1,3 +1,6 @@
+const callValueFeedBackArea = document.querySelector(".callValueFeedback")
+const putValueFeedBackArea = document.querySelector(".putValueFeedback")
+
 function updateModelValue(elementID, updatedValue) {
     const modelElement = "model".concat(elementID.charAt(0).toUpperCase() + elementID.slice(1))
     var existingValue = document.getElementById(modelElement)
@@ -20,9 +23,9 @@ function calculateModelValuation() {
         if (data.calculation_error) {
             console.log(data.calculation_error)
         }
-        console.log(data)
-        console.log(data.call_value)
-        console.log(data.put_value)
-        console.log(typeof(data.call_value))
+        callValueFeedBackArea.style.display = "block"
+        callValueFeedBackArea.innerHTML = `<h5>Call Value</h5><p>$${data.call_value}</p>`
+        putValueFeedBackArea.style.display = "block"
+        putValueFeedBackArea.innerHTML = `<h5>Put Value</h5><p>$${data.put_value}</p>`
     });
 }
