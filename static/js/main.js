18b7cfc8fc3843d60b6560cb0590da59b97c7db9
Edwyn Flores <edwynflores@gmail.com>
2024-08-08 20:47:44 -0700

    Whitenoise static files

---
+++ static/js/main.js
@@ -0,0 +1,38 @@
+const callValueFeedBackArea = document.querySelector(".callValueFeedback")
+const putValueFeedBackArea = document.querySelector(".putValueFeedback")
+
+function updateModelValue(elementID, updatedValue) {
+    const modelElement = "model".concat(elementID.charAt(0).toUpperCase() + elementID.slice(1))
+    var existingValue = document.getElementById(modelElement)
+
+    if (elementID.includes('Price')) {
+        existingValue.innerHTML = '$' + updatedValue
+    } else if (elementID.includes('Rate')) {
+        existingValue.innerHTML = updatedValue + '%'
+    } else {
+        existingValue.innerHTML = updatedValue
+    }
+}
+
+function calculateModelValuation() {
+    fetch("/model/perform-valuation", {
+        method: "POST",
+        body: JSON.stringify({
+            assetPrice: assetPrice.value,
+            strikePrice: strikePrice.value,
+            time: time.value,
+            volatility: volatility.value,
+            rfiRate: rfiRate.value
+        })
+    })
+    .then((res) => res.json())
+    .then((data) => {
+        if (data.calculation_error) {
+            console.log(data.calculation_error)
+        }
+        callValueFeedBackArea.style.display = "block"
+        callValueFeedBackArea.innerHTML = `<h4>Call Value</h4><h5>$${data.call_value}</h5>`
+        putValueFeedBackArea.style.display = "block"
+        putValueFeedBackArea.innerHTML = `<h4>Put Value</h4><h5>$${data.put_value}</h5>`
+    })
+}
---
+++ static/js/sidebar.js
@@ -0,0 +1,87 @@
+const assetPrice = document.getElementById("assetPrice")
+const assetPriceFeedBackArea = document.querySelector(".assetPriceFeedback")
+
+const strikePrice = document.getElementById("strikePrice")
+const strikePriceFeedBackArea = document.querySelector(".strikePriceFeedback")
+
+
+const time = document.getElementById("time")
+const timeFeedbackArea = document.querySelector(".timeFeedback")
+
+
+const volatility = document.getElementById("volatility")
+const volatilityFeedBackArea = document.querySelector(".volatilityFeedback")
+
+
+const rfiRate = document.getElementById("rfiRate")
+const rfiRateFeedBackArea = document.querySelector(".rfiRateFeedback")
+
+const variables = [{
+    "value": assetPrice,
+    "feedBackArea": assetPriceFeedBackArea
+},{
+    "value": strikePrice,
+    "feedBackArea": strikePriceFeedBackArea
+},{
+    "value": time,
+    "feedBackArea": timeFeedbackArea
+},{
+    "value": volatility,
+    "feedBackArea": volatilityFeedBackArea
+},{
+    "value": rfiRate,
+    "feedBackArea": rfiRateFeedBackArea
+}]
+
+variables.forEach(function(variable) {
+    variable.value.addEventListener("change", function(){
+        validateValue(variable.value, variable.feedBackArea)
+    })
+})
+
+// FIXME: Bug where you can input any number of decimals and 0s.
+function validateValue(value, feedBackArea) {
+    const defaultValue = 0.00
+    var inputValue = value.value
+
+    value.classList.remove("is-invalid")
+    feedBackArea.classList.remove("is-invalid")
+    feedBackArea.style.display = "none"
+
+    if (inputValue.length > 0 && Number(inputValue)) {
+        if (inputValue < defaultValue) {
+            fetch("/model/validate-value", {
+                method: "POST",
+                body: JSON.stringify({ value: inputValue })
+            })
+            .then((res) => res.json())
+            .then((data) => {
+                if (data.value_error) {
+                    value.classList.add("is-invalid")
+                    feedBackArea.style.display = "block"
+                    feedBackArea.innerHTML = `<p>${data.value_error}</p>`
+                }
+            })
+            return
+        }
+
+        const numOfDecimals = 2
+        value.value = parseFloat(inputValue).toFixed(numOfDecimals)
+        updateModelValue(value.id, value.value)
+        calculateModelValuation()
+        return
+    }
+
+    fetch("/model/validate-value", {
+        method: "POST",
+        body: JSON.stringify({ value: inputValue })
+    })
+    .then((res) => res.json())
+    .then((data) => {
+        if (data.value_error) {
+            value.classList.add("is-invalid")
+            feedBackArea.style.display = "block"
+            feedBackArea.innerHTML = `<p>${data.value_error}</p>`
+        }
+    })
+}
