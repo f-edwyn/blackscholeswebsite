5f525bc905aaef022d375ee3f593711c09dd98a4
Edwyn Flores <edwynflores@gmail.com>
2024-08-15 20:46:38 -0700

    Removed debugging logs

--- model/valuation.py
+++ model/valuation.py
@@ -20,11 +20,9 @@ def blackScholes(S, K, T, sigma, r):
 
         # Put and call value will equate to 0 for small values.
         if isinstance(call_value, np.floating):
-            print(call_value)
             call_value = "%.2f" % round(call_value, 2)
         if isinstance(put_value, np.floating):
             put_value = "%.2f" % round(put_value, 2)
-        print(call_value, put_value)
         return call_value, put_value
     except:
         raise Exception("Error occurred during calculation.")
